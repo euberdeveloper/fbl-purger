4a404d0858b3ae0a8ec53ff3636975e32c8c7fbf
euberdeveloper <euberdeveloper@gmail.com>
2021-05-09 12:06:49 +0200

    feat(uploader): add unique index
    Signed-off-by: euberdeveloper <euberdeveloper@gmail.com>

--- modules/purger/utils/uploader.py
+++ modules/purger/utils/uploader.py
@@ -1,22 +1,32 @@
-from pymongo import MongoClient
+from pymongo import MongoClient, ASCENDING
 from ...utils.logger import log
 
-#addindexline
-
 class Uploader:
+    def __check_collection_already_exists(self, force: bool) -> None:
+        if self.collection.count != 0:
+            if force:
+                log(f'{self.language} already exists: dropping')
+                self.collection.drop()    
+
+    def __add_unique_line_index(self) -> None:
+        self.collection.create_index([('line', ASCENDING)], name='lineIndex', unique=True)
+
     def __init__(self, language: str, threshold: int, dbname: str, force: bool):
+        # Open connection and get collection
         self.client = MongoClient()
         self.language = language
         self.database = self.client.get_database(dbname)
         self.collection = self.database.get_collection(language)
 
+        # Initialize buffer and other fields
         self.buffer = []
         self.threshold = threshold
 
-        if self.collection.count != 0:
-            if force:
-                log(f'{self.language} already exists: dropping')
-                self.collection.drop()          
+        # Check if collection already exists
+        self.__check_collection_already_exists(force)
+
+        # Add unique index to collection for field "line"
+        self.__add_unique_line_index() 
 
     def upload(self) -> None:
         if self.buffer:
